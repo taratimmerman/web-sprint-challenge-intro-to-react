efb56caabe20ac32faa55a9e950d68276918ed9d
taratimmerman <timmermantara@gmail.com>
2020-12-11 11:15:17 -0700

    status q

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Character(info) {
+    return (
+        <StyledChar className='character'>
+            <h2>{info.character.name}</h2>
+            <h2>{info.character.species}</h2>
+        </StyledChar>
+    );
+}
+
+const StyledChar = styled.div`
+    display: flex;
+`
